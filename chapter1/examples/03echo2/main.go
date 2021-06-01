4784c67fcfd56e404a0c384963b047d9db0c9faa
Lucas <lhbelfanti@gmail.com>
2021-06-01 07:48:13 -0300

    Added chapter 1 example 3 and 4.

---
+++ chapter1/examples/03echo2/main.go
@@ -0,0 +1,19 @@
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+// Page 6.
+
+// Echo2 prints its command-line arguments.
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	s, sep := "", ""
+	for _, arg := range os.Args[1:] {
+		s += sep + arg
+		sep = " "
+	}
+	fmt.Println(s)
+}
---
+++ chapter1/examples/04echo3/main.go
@@ -0,0 +1,15 @@
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+// Page 8.
+
+// Echo3 prints its command-line arguments.
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+	fmt.Println(strings.Join(os.Args[1:], " "))
+}

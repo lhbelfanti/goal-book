271af95bedec42309d9b0664cfefa6a0c1e3e8be
Lucas <lhbelfanti@gmail.com>
2021-06-04 08:16:12 -0300

    Added chapter 2 examples 1, 2, 3, 4 and 5.

---
+++ chapter2/examples/05tempconv1/conv.go
@@ -0,0 +1,10 @@
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+// Page 41.
+
+package tempconv
+
+// CToF converts a Celsius temperature to Fahrenheit.
+func CToF(c Celsius) Fahrenheit { return Fahrenheit(c*9/5 + 32) }
+
+// FToC converts a Fahrenheit temperature to Celsius.
+func FToC(f Fahrenheit) Celsius { return Celsius((f - 32) * 5 / 9) }

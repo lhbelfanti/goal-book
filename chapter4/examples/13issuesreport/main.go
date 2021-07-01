2050bcfa3358bdfe30ad6bcce3d97b85e1a32341
Lucas <lhbelfanti@gmail.com>
2021-07-01 00:46:06 -0300

    Added chapter 4 examples 13, 14 and 15.

---
+++ chapter4/examples/13issuesreport/main.go
@@ -0,0 +1,75 @@
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+// Page 113.
+
+// Issuesreport prints a report of issues matching the search terms.
+package main
+
+import (
+	"log"
+	"os"
+	"text/template"
+	"time"
+
+	"gitlab.com/lhbelfanti/goal-book/chapter4/examples/11github"
+)
+
+const templ = `{{.TotalCount}} issues:
+{{range .Items}}----------------------------------------
+Number: {{.Number}}
+User:   {{.User.Login}}
+Title:  {{.Title | printf "%.64s"}}
+Age:    {{.CreatedAt | daysAgo}} days
+{{end}}`
+
+func daysAgo(t time.Time) int {
+	return int(time.Since(t).Hours() / 24)
+}
+
+var report = template.Must(template.New("issuelist").
+	Funcs(template.FuncMap{"daysAgo": daysAgo}).
+	Parse(templ))
+
+func main() {
+	result, err := github.SearchIssues(os.Args[1:])
+	if err != nil {
+		log.Fatal(err)
+	}
+	if err := report.Execute(os.Stdout, result); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func noMust() {
+	report, err := template.New("report").
+		Funcs(template.FuncMap{"daysAgo": daysAgo}).
+		Parse(templ)
+	if err != nil {
+		log.Fatal(err)
+	}
+	//!-parse
+	result, err := github.SearchIssues(os.Args[1:])
+	if err != nil {
+		log.Fatal(err)
+	}
+	if err := report.Execute(os.Stdout, result); err != nil {
+		log.Fatal(err)
+	}
+}
+
+/*
+$ go build main
+$ ./main repo:golang/go is:open json decoder
+13 issues:
+----------------------------------------
+Number: 5680
+User:   eaigner
+Title:  encoding/json: set key converter on en/decoder
+Age:    750 days
+----------------------------------------
+Number: 6050
+User:   gopherbot
+Title:  encoding/json: provide tokenizer
+Age:    695 days
+----------------------------------------
+...
+*/
